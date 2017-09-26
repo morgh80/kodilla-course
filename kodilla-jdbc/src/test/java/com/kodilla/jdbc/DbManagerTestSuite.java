6ce22f300ad6be47e0631001ee8533608eced108
Aeronaut <aeronaut@MacBook-Pro-Aeronaut-2.local>
2017-09-26 08:01:06 +0200

    Excersise 12.6 ready for code review

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -29,7 +29,7 @@ public class DbManagerTestSuite {
 
         //Then
         int counter = 0;
-        while(rs.next()) {
+        while (rs.next()) {
             System.out.println(rs.getInt("ID") + ", " +
                     rs.getString("FIRSTNAME") + ", " +
                     rs.getString("LASTNAME"));
@@ -38,6 +38,37 @@ public class DbManagerTestSuite {
         rs.close();
         statement.close();
         Assert.assertEquals(5, counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery =
+                "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUMBER\n" +
+                        "FROM USERS U, POSTS P\n" +
+                        "WHERE U.ID = P.USER_ID\n" +
+                        "GROUP BY U.ID\n" +
+                        "HAVING COUNT(*) >= 2";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(
+                    rs.getString("FIRSTNAME") + ", " +
+                            rs.getString("LASTNAME")
+
+            );
+            counter++;
+        }
+        rs.close();
+        statement.close();
+
+        Assert.assertEquals(2, counter);
+
     }
 
 }
